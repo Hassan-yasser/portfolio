bed7b4b0b1e324d4aa8d5d470dbd0b0f160f4002
Hassan-yasser <hasan.yasser.alhossiny@gmail.com>
2025-01-12 20:08:01 +0200

    second commit

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,9 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  eslint: {
+    ignoreDuringBuilds: true, // لتعطيل ESLint أثناء عملية البناء
+  },
 };
 
 export default nextConfig;
